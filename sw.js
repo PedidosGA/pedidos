dd52c550ac1c6f9a6c3abe95c856f7576d20ed65
PedidosGA <walter.munnich@grupoandino.com.ar>
2026-09-22 20:53:50 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+/* Mínimo necesario para que Chrome ofrezca "Instalar". No guarda nada. */
+self.addEventListener('install', function () { self.skipWaiting(); });
+self.addEventListener('activate', function (e) { e.waitUntil(self.clients.claim()); });
+self.addEventListener('fetch', function () {});
